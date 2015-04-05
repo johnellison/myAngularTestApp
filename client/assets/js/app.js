22e529a9643d429f21e72e931741420f36cfde04
John Ellison <info@johnwebsites.com>
2015-04-04 21:08:53 -0600

    finish ch 2

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -12,6 +12,72 @@
   ])
     .config(config)
     .run(run)
+    .controller('MainCtrl', [
+      function() {
+        var self = this;
+        
+        var notes = [
+          {
+            id: 1,
+            label: 'First Note',
+            done: false,
+            someRandon: 31431
+          },
+          {
+            id: 2,
+            label: 'Second Note',
+            done: false
+          },
+          { 
+            id: 3,
+            label: 'Done Note',
+            done: true
+          },
+          { 
+            id: 4,
+            label: 'Fourth Note',
+            done: false
+          }
+        ];
+
+        self.notes1 = angular.copy(notes);
+        self.notes2 = angular.copy(notes);
+
+        self.changeNotes = function() {
+          notes = [
+            {
+              id: 1,
+              label: 'First Note',
+              done: false,
+              someRandon: 31431
+            },
+            {
+              id: 2,
+              label: 'Second Note',
+              done: false
+            },
+            { 
+              id: 3,
+              label: 'Done Note',
+              done: true
+            },
+            { 
+              id: 4,
+              label: 'Fourth Note',
+              done: false
+            }
+          ];
+          self.notes1 = angular.copy(notes);
+          self.notes2 = angular.copy(notes);
+        }
+
+        self.getNoteClass = function(status) {
+          return {
+            done: status,
+            pending: !status
+          }
+        }
+      }]);
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
